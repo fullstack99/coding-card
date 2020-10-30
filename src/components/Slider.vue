6029374c6db58734e00de43ab062f47d92b9469d
fullstack99 <success1989127@gmail.com>
2020-10-30 13:29:17 -0600

    create task

---
+++ src/components/Slider.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="slider-container">
+    <input
+      type="range"
+      class="slider-container__input"
+      min="1"
+      max="100"
+      :value="score"
+      :disabled="isEdit"
+      readonly="1"
+    />
+    <output
+      class="slider-container__output"
+      :class="
+        score > 60
+          ? 'slider-container__output--excellent'
+          : score > 30
+          ? 'slider-container__output--good'
+          : 'slider-container__output--normal'
+      "
+      :style="{ left: score + '%' }"
+    >
+      {{ score }}
+    </output>
+  </div>
+</template>
+<script>
+export default {
+  name: 'Slider',
+  props: {
+    score: Number,
+    isEdit: Boolean,
+  },
+};
+</script>
+<style lang="scss" scoped>
+.slider-container {
+  position: relative;
+  margin: 0 auto 30px;
+  &__input {
+    width: 100%;
+  }
+  &__output {
+    color: white;
+    position: absolute;
+    border-radius: 4px;
+    transform: translateX(-50%);
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    padding: 5px;
+    font-size: 12px;
+    &--excellent {
+      background-color: green;
+    }
+    &--good {
+      background-color: blue;
+    }
+    &--normal {
+      background-color: red;
+    }
+  }
+}
+</style>
---
+++ src/views/Cards.vue
@@ -0,0 +1,194 @@
+<template>
+  <div class="card-list">
+    <div v-for="card in cards" :key="card.id" class="card">
+      <div class="card__header">
+        <p class="card__title">{{ card.title }}</p>
+        <span
+          class="card__badge"
+          :class="
+            card.score > 60
+              ? 'card__badge--excellent'
+              : card.score > 30
+              ? 'card__badge--good'
+              : 'card__badge--normal'
+          "
+        >
+          {{ card.score }}
+        </span>
+      </div>
+
+      <div class="task-list" v-for="task in card.tasks" :key="task.id">
+        <p class="task-list__item">{{ task.content }}</p>
+      </div>
+      <SliderComponent :score="card.score" :isEdit="isEdit" />
+    </div>
+  </div>
+</template>
+
+<script>
+import SliderComponent from '@/components/Slider';
+
+export default {
+  name: 'Cards',
+  components: {
+    SliderComponent,
+  },
+  props: {
+    msg: String,
+  },
+  data() {
+    return {
+      cards: [
+        {
+          title: 'test',
+          id: 1,
+          score: 90,
+          tasks: [
+            {
+              id: 1,
+              content: 'test1',
+            },
+            {
+              id: 2,
+              content: 'test2',
+            },
+            {
+              id: 3,
+              content: 'test3',
+            },
+          ],
+        },
+        {
+          title: 'test2',
+          id: 2,
+          score: 30,
+          tasks: [
+            {
+              id: 1,
+              content: 'test1',
+            },
+            {
+              id: 2,
+              content: 'test2',
+            },
+            {
+              id: 3,
+              content: 'test3',
+            },
+          ],
+        },
+        {
+          title: 'test3',
+          id: 3,
+          score: 50,
+          tasks: [
+            {
+              id: 1,
+              content: 'test1',
+            },
+            {
+              id: 2,
+              content: 'test2',
+            },
+            {
+              id: 3,
+              content: 'test3',
+            },
+          ],
+        },
+        {
+          title: 'test4',
+          id: 4,
+          score: 70,
+          tasks: [
+            {
+              id: 1,
+              content: 'test1',
+            },
+            {
+              id: 2,
+              content: 'test2',
+            },
+            {
+              id: 3,
+              content: 'test3',
+            },
+          ],
+        },
+        {
+          title: 'test5',
+          id: 5,
+          score: 0,
+          tasks: [
+            {
+              id: 1,
+              content: 'test1',
+            },
+            {
+              id: 2,
+              content: 'test2',
+            },
+            {
+              id: 3,
+              content: 'test3',
+            },
+          ],
+        },
+      ],
+      isEdit: true,
+    };
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+
+<style lang="scss" scoped>
+.card-list {
+  display: flex;
+  justify-content: center;
+  flex-wrap: wrap;
+  .card {
+    border: 1px solid #3535353d;
+    border-radius: 5px;
+    box-shadow: 0px 5px 8px 0px rgba(43, 41, 42, 0.76);
+    margin: 30px;
+    padding: 30px;
+    max-width: 150px;
+    width: 100%;
+    &__header {
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+    }
+    &__title {
+      font-size: 14px;
+      font-weight: bold;
+    }
+    &__badge {
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      width: 30px;
+      height: 30px;
+      color: #ffffff;
+      &--excellent {
+        background-color: green;
+        border-radius: 15px;
+      }
+      &--good {
+        background-color: blue;
+        border-radius: 10px;
+      }
+      &--normal {
+        background-color: red;
+      }
+    }
+  }
+  .task-list {
+    &__item {
+      font-size: 12px;
+    }
+  }
+}
+</style>
